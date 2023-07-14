baee6a7fcc2a469cad57bc7084f5a19edac70be3
hajar298 <hajarih@code.edu.az>
2023-07-14 21:29:26 +0400

    addCourseIndex

---
+++ EduHome.App/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using EduHome.App.Context;
+using EduHome.App.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduHome.App.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly EduHomeDbContext _context;
+        public CourseController(EduHomeDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            HomeViewModel homeViewModel = new HomeViewModel();
+            homeViewModel.Courses = await _context.Courses.
+              Include(x => x.CourseCategory).
+              Where(x => !x.IsDeleted).ToListAsync();
+            homeViewModel.SocialMedias = await _context.SocialMedias.Where(x => !x.IsDeleted).ToListAsync();
+
+            return View(homeViewModel);
+        }
+    }
+}
