d9957713004d719037fbfa958208bcf444b6d30b
hajar298 <hajarih@code.edu.az>
2023-07-14 22:20:50 +0400

    addPartials

--- EduHome.App/Controllers/AboutController.cs
+++ EduHome.App/Controllers/AboutController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduHome.App.Context;
+using EduHome.App.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduHome.App.Controllers
 {
     public class AboutController : Controller
     {
-        public IActionResult Index()
+        private readonly EduHomeDbContext _context;
+        public AboutController(EduHomeDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            HomeViewModel homeViewModel = new HomeViewModel();
+            homeViewModel.Teachers = await _context.Teachers.
+              Include(x => x.teacherPositionCat).Include(x => x.SocialMedias).
+              Where(x => !x.IsDeleted).ToListAsync();
+            homeViewModel.SocialMedias = await _context.SocialMedias.Where(x => !x.IsDeleted).ToListAsync();
+
+            homeViewModel.Testinomials = await _context.Testinomials.Include(x => x.CourseCategory).Include(x => x.PositionCategory)
+              .Where(x => !x.IsDeleted).ToListAsync();
+
+            return View(homeViewModel);
         }
     }
 }
